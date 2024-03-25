0bff14978e99344b3753db9d451e3aa9336784ca
Elliot Su <sodinfeliz@gmail.com>
2024-03-25 14:55:40 +0800

    [FEAT] Add `restrict_arguments` decorator

--- pyerrorschema/__init__.py
+++ pyerrorschema/__init__.py
@@ -1,5 +1,6 @@
 from .base_error_schema import ErrorSchema
 from .fastapi_error_schema import FastAPIErrorSchema
+from .utils import restrict_arguments
 
 
-__all__ = ["FastAPIErrorSchema", "ErrorSchema"]
+__all__ = ["FastAPIErrorSchema", "ErrorSchema", "restrict_arguments"]
