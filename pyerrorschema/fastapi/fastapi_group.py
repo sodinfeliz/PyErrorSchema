09d55049b49025df82faeb6f9deb9c01d79e73d2
Elliot Su <sodinfeliz@gmail.com>
2024-08-29 17:52:24 +0800

    [FIX] Fix the static type issues

--- pyerrorschema/fastapi/fastapi_group.py
+++ pyerrorschema/fastapi/fastapi_group.py
@@ -1,7 +1,11 @@
-from pyerrorschema.err_base import ErrorSchema
+from typing import List, Optional
+
+from typing_extensions import Self, TypeVar
 
 from ..err_group import ErrGroup
-from .fastapi_base import FastAPIErrorSchema
+from .fastapi_base import ErrorSchema, FastAPIErrorSchema
+
+ErrorSchemaType = TypeVar("ErrorSchemaType", bound="ErrorSchema")
 
 
 class FastAPIErrGroup(ErrGroup):
@@ -14,14 +18,15 @@ class FastAPIErrGroup(ErrGroup):
 
     def _validate_instance(self, errs):
         """Check if the error schemas are instances of FastAPIErrorSchema."""
-        if errs is None: return
+        if errs is None:
+            return
         if isinstance(errs, list):
             if not all(isinstance(err, FastAPIErrorSchema) for err in errs):
-                raise ValueError("All elements in the list must be instances of FastAPIErrorSchema.")
+                raise ValueError("All elements must be instances of FastAPIErrorSchema.")
         elif not isinstance(errs, FastAPIErrorSchema):
             raise ValueError("The error schema must be an instance of FastAPIErrorSchema.")
-    
+
     def append_loc(self, loc: str) -> None:
-        """Append a location to all error schemas in the group."""
+        """Globally append a location to all error schemas in the group."""
         for err in self.error_schemas:
             err.loc.append(loc)
